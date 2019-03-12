6ca9f3ce26089da9033d8ebd6de3ab88f5550abb
Jay Sokolowski <jaysoko@gmail.com>
2019-03-12 00:20:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,16 @@
 class School
     attr_accessor :name
-sc  student_roster = {}
+     student_roster = {}
 
 def initialize(name)
    @name = name 
 end
 
-   
-end
-
-
-
 def add_student(name, grade)
-
 end
 
 
 
 
 end
+
