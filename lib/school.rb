d99026143df678408d08b532d6da6b1ff730d09f
Jay Sokolowski <jaysoko@gmail.com>
2019-03-12 00:43:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,37 @@
 class School
-    attr_accessor :name
-     student_roster = {}
+    attr_accessor :name :roster
+    roster = {}
 
 def initialize(name)
    @name = name 
+   @roster = {}
 end
 
 def add_student(name, grade)
+   @roster[grade] = [] unless @roster[grade]
+   @roster[grade] << name
 end
+
+def grade(grade)
+    @roster[grade]
+  end
+
+  def name(grade)
+    @roster[grade]
+  end
+
+  def sort_pre_refactor
+    sorted = {}
+    @roster.each do|key, values|
+      sorted[key] ||= values.sort
+    end
+    sorted
+  end
+
+  def sort
+    @roster.map {|k, v| v.sort!}
+    @roster
+  end
 
 
 
